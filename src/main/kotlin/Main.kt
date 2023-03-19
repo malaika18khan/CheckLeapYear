bfdc8be0326c8025eda3c75d5f1b5a4486f980cd
malaika18khan <malaika18khan@gmail.com>
2023-03-19 23:51:26 +0500

    Initial commit of Bytewise first week task

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,18 @@
+fun main() {
+    println("Enter the year:")
+    val year = readLine()?.toInt()
+    var leap = false
+
+    if (year != null) {
+        if (year % 4 == 0) {
+            if (year % 100 == 0) {
+                leap = year % 400 == 0
+            } else
+                leap = true
+        } else
+            leap = false
+    }
+
+    println(if (leap) "$year is a leap year." else "$year is not a leap year.")
+
+}
